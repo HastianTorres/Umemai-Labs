871215b2a3bc5995a658944dd2d18064654936f4
HastianTorres <142507148+HastianTorres@users.noreply.github.com>
2026-03-19 14:11:44 -0300

    Refactor toggleMenu for sidebar menu animation
    Refactored toggleMenu function to use style.left for menu positioning.

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+function toggleMenu(){
 
+let menu = document.getElementById("sidebar")
+
+if(menu.style.left === "0px"){
+
+menu.style.left = "-220px"
+
+}else{
+
+menu.style.left = "0px"
+
+}
+
+}
 
 
 document.getElementById("themeToggle")
