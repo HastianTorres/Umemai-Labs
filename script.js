7933b0d84e5e5637f0cf04c27b42e8b68d724a50
HastianTorres <142507148+HastianTorres@users.noreply.github.com>
2026-03-19 14:03:14 -0300

    Refactor toggleMenu function and improve menu handling

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function toggleMenu(){
 
-let menu = document.getElementById("sidebar")
-
-if(menu.style.left === "0px"){
-
-menu.style.left = "-220px"
-
-}else{
-
-menu.style.left = "0px"
-
-}
-
-}
 
 
 document.getElementById("themeToggle")
